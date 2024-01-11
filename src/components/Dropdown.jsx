b68ab73e14eb9dac4b1c03f2a262affef2383eca
zakuurr <ezakurnia50@gmail.com>
2024-01-11 16:14:54 +0700

    test digital

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,75 +1,73 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-
+import { Select } from 'antd';
 
 function Dropdown() {
   const [data, setData] = useState([]);
-  const [selectedData, setSelectedData] = useState(null);
-  const [filter, setFilter] = useState([]);
-
+  const [selectedValue, setSelectedValue] = useState(null);
 
   useEffect(() => {
     axios.get('https://pokeapi.co/api/v2/berry-firmness/')
       .then(response => {
         const data = response.data.results;
-        console.log(data);
         setData(data);
-        setFilter(data);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
   }, []);
 
-
-  const handleSelectChange = (e) => {
-    const selectedValue = e.target.value;
-    setSelectedData(selectedValue);
-
-  
-    const sort = [...filter].sort((a, b) => a.name.localeCompare(b.name));
-    setFilter(sort);
+  const onChange = (value, option) => {
+    console.log(`selected ${value}`);
+    setSelectedValue(option);
   };
-  
+
+  const onSearch = (value) => {
+    console.log('search:', value);
+  };
+
+  const filterOption = (input, option) =>
+    (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
+
   return (
     <>
       <section className="h-full max-h-full min-h-full sm:py-20 lg:py-20">
         <div className="px-6 mx-auto rounded-lg sm:px-20 lg:px-20">
           <div className="flex items-center justify-center">
             <h1 className="mt-3 text-2xl font-semibold text-gray-900">
-             Tes Dropdown
+              Tes Dropdown
             </h1>
           </div>
           <div className="max-w-2xl mx-auto mt-8 rounded-lg md:mt-12">
             <div className="bg-gray-200 rounded-lg shadow-lg">
               <div className="justify-center px-4 py-6 sm:px-8 sm:py-10">
-              <select className="w-full max-w-xs select select-bordered" onChange={handleSelectChange} value={selectedData || ''}>
-              <option selected>Select</option>
-              {data.map(item => (
-          <option key={item.name} value={item.name}>{item.name}</option>
-        ))}
-            </select>
-
-            <h2 className="mt-3">Output:</h2>
-          <ul>
-         
-            {filter.map(item => (
-              <li key={item.name} style={{ display: item.name === selectedData ? 'block' : 'none' }}>
-                {item.name} <br/>
-                {item.url}
-                </li>
-            ))}
-          </ul>
+                <Select
+                  showSearch
+                  placeholder="Select data"
+                  optionFilterProp="children"
+                  onChange={onChange}
+                  onSearch={onSearch}
+                  filterOption={filterOption}
+                  options={data
+                    .sort((a, b) => a.name.localeCompare(b.name))
+                    .map(item => ({ value: item.url, label: item.name }))
+                  }
+                />
+                
+                {selectedValue && (
+                  <div className="mt-3">
+                    <h1>Output</h1>
+                    <strong>Name:</strong> {selectedValue.label} <br />
+                    <strong>URL:</strong> {selectedValue.value}
+                  </div>
+                )}
               </div>
-
-              
             </div>
           </div>
-         
         </div>
       </section>
     </>
   );
-}
+};
 
 export default Dropdown;
